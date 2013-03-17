d921938ca69e1751cdcb816f9198874666b934ce
Thomas Sileo <thomas.sileo@gmail.com>
2013-03-17 20:21:56 +0100

    BakSyncer bugfix

--- bakthat/sync.py
+++ bakthat/sync.py
@@ -87,6 +87,8 @@ class BakSyncer():
         sync_ts = r.json().get("sync_ts")
         for newbackup in to_insert_in_bakthat:
             sqlite_backup = Backups.match_filename(newbackup["stored_filename"], newbackup["backend"])
+            print sqlite_backup
+            print sqlite_backup._data
             if sqlite_backup:
                 log.debug("Create backup {0}".format(newbackup))
                 Backups.create(**newbackup)
